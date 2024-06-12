a10a41153121da12a4a51382c5ccce9dd5fa6063
Erbynn John <john.erbynn@gmail.com>
2024-06-12 08:30:43 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace StatlerWaldorfCorp.WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
